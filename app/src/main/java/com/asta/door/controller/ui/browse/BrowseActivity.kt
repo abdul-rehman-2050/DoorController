a3484bbd7e257c188901e1a65db26b47de6fcbe7
Abdul Rehman <supermind2002@gmail.com>
2022-08-17 17:26:03 +0500

    lkjlkjl

--- app/src/main/java/com/asta/door/controller/ui/browse/BrowseActivity.kt
+++ app/src/main/java/com/asta/door/controller/ui/browse/BrowseActivity.kt
@@ -3,6 +3,7 @@ package com.asta.door.controller.ui.browse
 import android.content.Intent
 import android.net.Uri
 import android.view.View
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.asta.door.controller.AppSettings
@@ -17,7 +18,22 @@ class BrowseActivity : BaseActivity<ActivityBrowseBinding>(
         binding.webView.apply {
             isVerticalScrollBarEnabled = false
             settings.javaScriptEnabled = true
+            settings.setJavaScriptEnabled(true)
+
+            settings.setLoadWithOverviewMode(true);
+            settings.setUseWideViewPort(true);
+            settings.setBuiltInZoomControls(true);
+            settings.setPluginState(WebSettings.PluginState.ON);
+            val newUA =
+                "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.4) Gecko/20100101 Firefox/4.0"
+            settings.setUserAgentString(newUA)
+
+
+
+
             setLayerType(View.LAYER_TYPE_HARDWARE, null)
+
+
             webViewClient = object : WebViewClient() {
                 @Deprecated("Deprecated in Java")
                 override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
